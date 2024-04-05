4bf6c6bc3b62e89058ace8d25b970fdbe2c44601
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-04-05 12:54:01 +0200

    ✨ feat(UX): Run SS Code & Use of FileSystem

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,30 @@
+import fs from "fs/promises";
+import path from "path";
+
 function HomePage(props) {
   const { products } = props;
 
   return (
     <ul>
-      {products.map((product) => <li key={product.id}>{product.title}</li>)}
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
 }
 
+// ______________________________________________________________________
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: 'p1', title: 'Product 1' }],
+      // products: [{ id: "p1", title: "Product 1" }],
+      products: data.products,
     },
-  }
+  };
 }
 
 export default HomePage;
