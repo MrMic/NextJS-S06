6d1994338ae067bcbeae6c94eae60881b1befe84
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-04-09 12:20:23 +0200

    💫 feat: getStaticProps Configuration Options

--- pages/index.js
+++ pages/index.js
@@ -14,12 +14,26 @@ function HomePage(props) {
 }
 
 // ______________________________________________________________________
-export async function getStaticProps() {
+export async function getStaticProps(context) {
   console.log("🪚 : Re-Generating");
 
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
