ef0d7c079aebb8982fb4a8d31dc1c4f4766b61d3
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-04-10 12:50:40 +0200

    ✨ feat: Working w/ fallback attribute

--- pages/[pid].js
+++ pages/[pid].js
@@ -6,6 +6,11 @@ import { Fragment } from "react";
 function ProductDetailPage(props) {
   const { loadedProduct } = props;
 
+  // NOTE: This will only be executed if fallback: true
+  // if (!loadedProduct) {
+  //   return <p>Loading...</p>;
+  // }
+
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -37,10 +42,12 @@ export async function getStaticPaths() {
   return {
     paths: [
       { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
+      // { params: { pid: "p2" } },
+      // { params: { pid: "p3" } },
     ],
-    fallback: false,
+    // fallback: false,
+    // fallback: true,
+    fallback: "blocking",
   };
 }
 
